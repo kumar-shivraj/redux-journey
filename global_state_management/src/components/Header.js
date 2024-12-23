6d5c31c9d1203cf610852e3a60aab41ead3ba8ed
kumar-shivraj <shivrajkumar11math@gmail.com>
2024-12-23 09:07:56 +0530

    global state management: add logic for fourth essential aspect of global state management : way to get the updated state : do it by maintaining the local state

--- global_state_management/src/components/Header.js
+++ global_state_management/src/components/Header.js
@@ -1,12 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import SwitchToDarkIcon from "../images/icon-moon.svg";
 import SwitchToLightIcon from "../images/icon-sun.svg";
 import store from "../todoStore";
 
+// getting subscribed to the event : when state is updated, component should effect with the latest state
+/*
+  HACKY!
+
+  Approach 1 : Maintaining the local State :
+  As we know that when local state is getting updated, the component is going to re-render.
+
+*/
 // const Header = ({ isDarkMode, setIsDarkMode }) => {
 const Header = () => {
   // here isDarkMode is not required by porp as we can directly read it from global store
   const isDarkMode = store.getState().isDarkMode;
+  console.log("isDarkMode inside Header : ", isDarkMode);
+
+  //  maintaining the local state in order to keep the component in sync with the global state
+  const [localIsDarkMode, setLocalIsDarkMode] = useState(isDarkMode);
 
   const getThemeIcon = (isDarkMode) => {
     return isDarkMode ? SwitchToLightIcon : SwitchToDarkIcon;
@@ -18,6 +30,10 @@ const Header = () => {
 
     // Dispatch the function
     store.dispatch();
+
+    // after dispatch, the store will be updated. So we can take the latest state from the store and update the local state
+    // store will return the new value of the state
+    setLocalIsDarkMode(store.getState().isDarkMode);
   };
 
   return (
