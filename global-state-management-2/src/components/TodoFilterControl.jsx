390e87db3e89aeff4382e9d29f16f9661f2a7af2
kumar-shivraj <shivrajkumar11math@gmail.com>
2024-12-27 14:21:13 +0530

    global state management 2 : initialize another global state and read it through global state

--- global-state-management-2/src/components/TodoFilterControl.jsx
+++ global-state-management-2/src/components/TodoFilterControl.jsx
@@ -1,31 +1,32 @@
 import React from "react";
-import { useState } from "react"
+import { useState } from "react";
+import store from "../todoStore";
 
 let statuses = [
-    { id: "all", title: "All" },
-    { id: "active", title: "Active" },
-    { id: "completed", title: "Completed" }
-]
+  { id: "all", title: "All" },
+  { id: "active", title: "Active" },
+  { id: "completed", title: "Completed" },
+];
 
 const TodoFilterControl = () => {
-    let filterStatus = "all";
+  // let filterStatus = "all";
+  const filterStatus = store.getState().filterStatus;
 
-    return (
-        <div className="control-btn group">
-            {statuses.map((status, idx) => {
-                return (
-                    <button
-                        id={status.id}
-                        className={filterStatus === status.id ? "btn active" : "btn"}
-                        key={idx}
-                    >
-                        {status.title}
-                    </button>
-                )
-            }
-            )}
-        </div>
-    );
+  return (
+    <div className="control-btn group">
+      {statuses.map((status, idx) => {
+        return (
+          <button
+            id={status.id}
+            className={filterStatus === status.id ? "btn active" : "btn"}
+            key={idx}
+          >
+            {status.title}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default TodoFilterControl;
--- global-state-management-2/src/todoStore.js
+++ global-state-management-2/src/todoStore.js
@@ -1,14 +1,16 @@
 import createStore from "./library/createStore";
 
 const initialState = {
-    "isDarkMode": false,
-}
+  isDarkMode: false,
+  filterStatus: "active",
+  // filterStatus: "all",
+};
 
 const toggleDarkMode = (state) => {
-    console.log(state.isDarkMode);
-    state.isDarkMode = !(state.isDarkMode);
-    console.log(state.isDarkMode);
-}
+  // console.log(state.isDarkMode);
+  state.isDarkMode = !state.isDarkMode;
+  // console.log(state.isDarkMode);
+};
 
 const store = createStore(initialState, toggleDarkMode);
 
