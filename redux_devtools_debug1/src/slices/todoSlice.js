466645406155ee7e29725f6ad63bf2b845942a8a
kumar-shivraj <shivrajkumar11math@gmail.com>
2025-01-08 06:43:59 +0530

    redux devtools debug 1: fix the default filter status to all instead of completed

--- redux_devtools_debug1/src/slices/todoSlice.js
+++ redux_devtools_debug1/src/slices/todoSlice.js
@@ -4,7 +4,8 @@ import data from "../data/initialTodos";
 const todoSlice = createSlice({
   name: "todoReducer",
   initialState: {
-    filterStatus: "completed",
+    // filterStatus: "completed",
+    filterStatus: "all",
     todos: data,
   },
   reducers: {
